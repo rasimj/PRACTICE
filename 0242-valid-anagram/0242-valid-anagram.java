93a4d0405237330d50188a99443296599597a186
AHAMED RASIM ALI J <ahamedrasimali2002@gmail.com>
2026-04-17 18:52:24 +0530

    Time: 2 ms (99.59%), Space: 44.2 MB (84.21%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public static boolean isAnagram(String s, String t) {
+
+        int m = s.length();
+        int n = t.length();
+
+        if(m != n){
+            return false;
+        }
+
+        int count[] = new int[26];
+
+        for(int i = 0; i < m; i++){
+            count[s.charAt(i) - 'a']++;
+        }
+
+        for(int i = 0; i < n; i++){
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for(int i = 0; i < count.length-1; i++){
+            if(count[i] != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
