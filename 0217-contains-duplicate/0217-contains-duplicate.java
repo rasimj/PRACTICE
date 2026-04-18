3068dbeffeae5a4fecea47831f6cbd4751f241f6
AHAMED RASIM ALI J <ahamedrasimali2002@gmail.com>
2026-04-18 19:05:20 +0530

    Time: 22 ms (30.94%), Space: 97.8 MB (10.76%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(nums[i])) {
+                return true;
+            } else {
+                hm.put(nums[i], i);
+            }
+
+        }
+        return false;
+    }
+}
