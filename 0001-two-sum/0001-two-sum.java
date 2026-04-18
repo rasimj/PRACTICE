fba6caa815497011a04a06f2017b33f8008f330a
AHAMED RASIM ALI J <ahamedrasimali2002@gmail.com>
2026-04-18 18:12:10 +0530

    Time: 2 ms (99.28%), Space: 47.2 MB (44.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,5 @@
 class Solution {
 
-    public static void main(String[] args)
-    {
-        int[] nums = { 2,7,11,15};
-        int target = 9;
-        System.out.println(twoSum(nums,target));
-    }
-
-
-
     public static int[] twoSum(int[] nums, int target) {
 
         HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
